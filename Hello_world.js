25f9f2b03e5ca1dfed668772498bd04aded1dcdf
Hayder272727 <haydernh@gmail.com>
2025-10-07 19:15:57 -0400

    Project update - Added CSS and assets

---
+++ Hello_world.js
@@ -0,0 +1,12 @@
+let greeting = "Hello World";
+console.log(greeting);
+let greeting = "Hello World";
+console.log(greeting);
+
+function addNumbers(a, b) {
+    return a + b;
+}
+
+let sum = addNumbers(5, 7);
+console.log("The sum is: " + sum);
+
